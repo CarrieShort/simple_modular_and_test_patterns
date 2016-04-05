d21df699021252904d335576dddb91643ea1ec8c
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-05 10:07:24 -0700

    Bonus 2: update tests to use inputUser instead of name, setting the value of process.argv[2] with a placeholder

--- carrie-short/greet.js
+++ carrie-short/greet.js
@@ -1,6 +1,4 @@
 'use strict';
 const Greeting = require(__dirname + '/lib/greeting.js');
 
-for (var i = 2; i < process.argv.length; i++){
-  console.log(new Greeting(process.argv[i]).greet());
-}
+console.log(new Greeting(process.argv[2]).greet());
--- carrie-short/test/greeting_test.js
+++ carrie-short/test/greeting_test.js
@@ -2,14 +2,16 @@ const chai = require('chai');
 const expect = chai.expect;
 const Greeting = require(__dirname + '/../lib/greeting.js');
 
-const name = 'Jake'
-const greeting = new Greeting(name);
+process.argv[2] = 'Jake';
+const inputUser = process.argv[2];
+debugger;
+const greeting = new Greeting(inputUser);
 
 describe('greeting', ()=>{
   it('should have a name',()=>{
-    expect(greeting.name).to.eql(name);
+    expect(greeting.name).to.eql(inputUser);
   });
   it('should greet the name with a friendly hello',()=>{
-    expect(greeting.greet()).to.eql('hello ' + name);
+    expect(greeting.greet()).to.eql('hello ' + inputUser);
   });
 });
