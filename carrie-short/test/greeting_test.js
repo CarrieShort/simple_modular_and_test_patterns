7a0090ae6db10daee9afc62164da935b8f5adab6
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-04 22:38:25 -0700

    add test for name

--- carrie-short/test/greeting_test.js
+++ carrie-short/test/greeting_test.js
@@ -5,6 +5,8 @@ const Greeting = require(__dirname + '/../lib/greeting.js');
 const greeting = new Greeting('Jake');
 
 describe('greeting', ()=>{
-  it('should have a name');
+  it('should have a name',()=>{
+    expect(greeting.name).to.eql('Jake');
+  });
   it('should greet the name with a friendly hello');
 });
